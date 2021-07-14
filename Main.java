eeaadb444157672ddfa787fa8b90d2caa327f43b
romanskaskiv <56167439+romanskaskiv@users.noreply.github.com>
2021-07-14 12:02:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-//
+
 class Main {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int x = sc.nextInt();
-        System.out.println("Is number palindrome: " + isPalindrome(x));
-        System.out.println("Reversed number: " + reverseDigits(x));
-
-
+        int y = sc.nextInt();
+        System.out.println(a + " is a palindrome? -> " + isPalindrome(a));
+        System.out.println(a + " with reversed digits? -> " + reverseDigits(a));
+        System.out.println(x + " / " + y + " ? -> " + divideInts(x, y));
+        System.out.println("Sqrt of " + b + " ? -> " + mySqrt(b));
     }
 
     public static boolean isPalindrome(int x) {
@@ -40,6 +40,39 @@ class Main {
         return isNegative ? reversed * -1 : reversed;
     }
 
+    public static int divideInts(int x, int y) {
+        int result = 0;
+        var xIsNegative = x < 0;
+        if (xIsNegative) {
+            x = -x;
+        }
+        var yIsNegative = y < 0;
+        if (yIsNegative) {
+            y = -y;
+        }
+        while (x - y >= 0) {
+            result += 1;
+            x -= y;
+        }
+
+        if ((xIsNegative && !yIsNegative) || (!xIsNegative && yIsNegative)) {
+            result = -result;
+        }
+
+        return result;
+    }
+
+    public static int mySqrt(int number) {
+        double temp;
+
+        double sr = number / 2;
+
+        do {
+            temp = sr;
+            sr = (temp + (number / temp)) / 2;
+        } while ((temp - sr) != 0);
+
+        return (int)sr;
+    }
+
 }
-
-
